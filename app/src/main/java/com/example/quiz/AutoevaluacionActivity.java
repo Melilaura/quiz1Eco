a98b502117ee71d361a8a0ce5d3df47b982bd5c0
Melilaura <orozcoveralauramelissa@gmail.com>
2021-09-16 13:23:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/quiz/AutoevaluacionActivity.java
@@ -0,0 +1,14 @@
+package com.example.quiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AutoevaluacionActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autoevaluacion);
+    }
+}
